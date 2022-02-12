1d777f56eb3ececee53b44fcef35240ebf7276bc
kevin <you@example.com>
2022-02-12 15:02:34 +0300

    Updated Files

---
+++ Static/Js/patient_portal.js
@@ -0,0 +1,44 @@
+const open_profile = document.querySelector("#bold");
+const profile = document.querySelector(".profile");
+
+open_profile.addEventListener("click", () => {
+  profile.classList.toggle("show-profile");
+});
+
+function openView(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
+
+const close_alert = document.querySelectorAll("#close");
+
+close_alert.forEach((p) => {
+  p.addEventListener("click", () => {
+    p.parentElement.style.display = "none";
+  });
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("actives");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
